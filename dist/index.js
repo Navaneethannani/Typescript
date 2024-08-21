e559bdbadcae3b22ee0d6a5f34fcfd14714a829f
Navaneethannani <navaneethanproff@gmail.com>
2024-08-21 15:04:38 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+let num1 = 50;
+let num2 = 10;
+function multiple(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiple(num1, num2), "multiple");
+let str = "nani";
+console.log(str, "name");
+let arr = ["nani", "games"];
+console.log(arr, "array");
+let tuple = ["nani", 2];
+console.log(tuple, "combine");
+let person = {
+    name: "nani",
+    age: 22
+};
+console.log(person, "person");
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+let num1 :number = 50
+let num2 :number = 10
+
+function multiple(num1:number , num2: number): number{
+    return num1 * num2
+}
+console.log(multiple(num1, num2), "multiple");
+
+
+let str :string = "nani"
+
+console.log(str,"name")
+
+let arr:string[] = ["nani", "games"]
+
+console.log(arr,"array")
+
+let tuple:[string, number] = ["nani" , 2]
+
+console.log(tuple,"combine")
+
+interface Person {
+    name : string,
+    age : number
+}
+
+let person : Person ={
+    name: "nani",
+    age: 22
+}
+
+console.log(person,"person")
