8359a010c6890f25b3b05ca528d56272a59eff31
Navaneethannani <navaneethanproff@gmail.com>
2024-09-03 10:45:04 +0530

    classes

--- dist/classes.js
+++ dist/classes.js
@@ -28,3 +28,39 @@ class Animal {
         return this.species;
     }
 }
+const species = new Animal('dog', 22, 'german');
+console.log(species.name);
+// console.log(species.age) //private only show inside the class so it will through error
+// console.log(species.species) // protected only used inside the class or inheritance class
+//Inheritance
+class Human {
+    constructor(fName, lName) {
+        this.fName = fName;
+        this.lName = lName;
+    }
+    getFullName() {
+        return `${this.fName} ${this.lName}`;
+    }
+    describe() {
+        return `This is ${this.getFullName()}.`;
+    }
+}
+class Employee extends Human {
+    constructor(fName, lName, jobTitle) {
+        super(fName, lName);
+        this.jobTitle = jobTitle;
+        Employee.headCount++;
+    }
+    describe() {
+        return super.describe() + `I am a ${this.jobTitle}`;
+    }
+    static getHeadCount() {
+        return Employee.headCount;
+    }
+}
+Employee.headCount = 0;
+let employee = new Employee("nani", "saga", "FullStack Developer");
+let Navi = new Employee("Navi", "abc", "Back-end Developer");
+console.log(employee.getFullName());
+console.log(employee.describe());
+console.log(Employee.getHeadCount());
--- src/classes.ts
+++ src/classes.ts
@@ -39,9 +39,50 @@ class Animal {
     private getAge(): number {
         return this.age;
     }
-
+    
     protected getSpecies(): string {
         return this.species;
     }
 }
 
+const species = new Animal('dog',22,'german');
+console.log(species.name)
+// console.log(species.age) //private only show inside the class so it will through error
+// console.log(species.species) // protected only used inside the class or inheritance class
+
+//Inheritance
+class Human {
+    constructor(private fName: string,private lName: string){}
+
+    getFullName(): string {
+        return `${this.fName} ${this.lName}`
+    }
+    describe(): string{
+        return `This is ${this.getFullName()}.`
+    }
+}
+
+class Employee extends Human{
+    private static headCount = 0;
+    constructor(
+        fName : string,
+        lName : string,
+        public jobTitle: string){
+        super(fName,lName);
+        Employee.headCount++;
+    }
+    describe(): string {
+        return super.describe() + `I am a ${this.jobTitle}`
+    }
+    public static getHeadCount(){
+        return Employee.headCount;
+    }
+
+}
+
+let employee = new Employee("nani", "saga","FullStack Developer")
+let Navi = new Employee("Navi","abc","Back-end Developer")
+
+console.log(employee.getFullName())
+console.log(employee.describe())
+console.log(Employee.getHeadCount())
